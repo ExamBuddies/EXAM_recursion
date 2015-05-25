9fc8ea0c757595b35724478fccf4f8c3be1d6e64
DAT14V1MadsPelt <mads3166@stud.kea.dk>
2015-05-25 14:09:33 +0200

    small fix

--- src/recursion/Recursion.java
+++ src/recursion/Recursion.java
@@ -3,7 +3,7 @@ package recursion;
 public class Recursion {
 	// Factorial (falkultet) findes ved at gange alle tal fra og med n til 1. Derfor kan vi gøre det rekursivt:
 	public static int factorial(int n){
-		if(n<=1){
+		if(n == 1){
 			return 1; 
 		}
 		return n * factorial(n-1);
