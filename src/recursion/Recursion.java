f72ab54c213d2b8df5d506013c76b5eba475dbeb
DAT14V1MadsPelt <mads3166@stud.kea.dk>
2015-05-25 13:29:22 +0200

    first commit

---
+++ src/recursion/Recursion.java
@@ -0,0 +1,25 @@
+package recursion;
+
+public class Recursion {
+	// Factorial (falkultet) findes ved at gange alle tal fra og med n til 1. Derfor kan vi gøre det rekursivt:
+	public static int factorial(int n){
+		if(n<1){
+			return 1; 
+		}
+		return n * factorial(n-1);
+	}
+	
+	// Find nummer n fibonaccital.
+	public static long fibonacci(int n){
+		if(n<=1){
+			return n;
+		}
+		else{
+			return fibonacci(n-1) + fibonacci(n-2);
+		}
+	}
+	
+	public static void printFileSystem(){
+		
+	}
+}
---
+++ src/recursion/RecursionDemo.java
@@ -0,0 +1,21 @@
+package recursion;
+
+public class RecursionDemo {
+
+	public static void main(String[] args) {
+		
+		
+//		System.out.println(Recursion.factorial(3));
+//		System.out.println(Recursion.factorial(4));
+//		System.out.println(Recursion.factorial(5));
+//		System.out.println(Recursion.factorial(6));
+		
+		System.out.println(Recursion.fibonacci(2));
+		System.out.println(Recursion.fibonacci(3));
+		System.out.println(Recursion.fibonacci(4));
+		System.out.println(Recursion.fibonacci(5));
+		System.out.println(Recursion.fibonacci(6));
+		System.out.println(Recursion.fibonacci(7));
+	}
+
+}
