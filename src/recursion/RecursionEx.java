97ea422dd12238a5cc5304aad1810398e5d7bef6
DAT14V1 - Casper Vejdiksen <casp539b@stud.kea.dk>
2015-05-30 17:14:30 +0200

    Create RecursionEx.java

---
+++ src/recursion/RecursionEx.java
@@ -0,0 +1,23 @@
+public class RecursionEx {
+
+	public static void main(String[] args) {
+
+		System.out.println(factor(4));
+	}
+
+	public static int factor(int n){
+
+		if (n == 1) {
+			System.out.print( n + " = ");
+			return 1;
+		}
+
+		else {
+			System.out.print(n + " x ");
+			return n * factor(n-1);
+
+		}	
+
+	}
+
+}
